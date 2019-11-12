042a962153fa260645e2728665ece57161e3cee5
Paalar <paaledward@gmail.com>
2019-11-12 20:29:50 +0100

    Add utils for window and localstorage functions

---
+++ src/utils/localStorage.ts
@@ -0,0 +1,29 @@
+import moment from 'moment';
+
+const LS_SPOTIFY_TOKEN = 'SPOTIFY_TOKEN';
+const LS_SPOTIFY_TOKEN_EXPIRATION = 'SPOTIFY_TOKEN_EXPIRATION';
+
+export const SPOTIFY_TOKEN = (): string | null => localStorage.getItem(LS_SPOTIFY_TOKEN);
+export const SPOTIFY_TOKEN_EXPIRATION = (): string | null => (
+  localStorage.getItem(LS_SPOTIFY_TOKEN_EXPIRATION)
+);
+export const setSpotifytoken = (token: string): void => {
+  localStorage.setItem(LS_SPOTIFY_TOKEN, token);
+  localStorage.setItem(LS_SPOTIFY_TOKEN_EXPIRATION, moment().add(1, 'hours').toString());
+};
+export const clearSpotifyStorage = (): void => {
+  localStorage.removeItem(LS_SPOTIFY_TOKEN_EXPIRATION);
+  localStorage.removeItem(LS_SPOTIFY_TOKEN);
+};
+export const spotifyStorageExists = (): boolean => (
+  !(SPOTIFY_TOKEN_EXPIRATION() === null || SPOTIFY_TOKEN_EXPIRATION() === null)
+);
+export const isSpotifyTokenExpired = (): boolean|null => {
+  const token = SPOTIFY_TOKEN_EXPIRATION();
+  if (token !== null) {
+    const tokenExpiration = moment(token);
+    const currentTime = moment();
+    return tokenExpiration.diff(currentTime) < 0;
+  }
+  return null;
+};
---
+++ src/utils/window.ts
@@ -0,0 +1,5 @@
+const spotifyAccessToken = (): string => window.location.hash.split('&').filter((value) => value.includes('access_token'))[0];
+
+export const urlHasSpotifyAccessToken = (): boolean => spotifyAccessToken() !== undefined;
+
+export const getSpotifyAccessToken = (): string => spotifyAccessToken().split('=')[1];
