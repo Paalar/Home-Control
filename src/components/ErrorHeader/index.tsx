3c54917aae9b40ad60328666d26d3d5ec165c84c
Paalar <paaledward@gmail.com>
2019-11-12 20:28:28 +0100

    remove unused lines and files

--- src/components/ErrorHeader/index.tsx
+++ src/components/ErrorHeader/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect, useState } from 'react';
+import React, { FunctionComponent, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import Presentational from './Presentational';
 import './errorHeader.scss';
