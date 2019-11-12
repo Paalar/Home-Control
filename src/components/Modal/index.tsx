958a1d81c48d2c64a3e60a8ae6ad57a6d3022b1f
Paalar <paaledward@gmail.com>
2019-11-12 15:55:03 +0100

    Update how modal workes, more simple

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -5,27 +5,17 @@ import Presentational from './presentational';
 
 interface Props {
   children: JSX.Element;
-  title: string;
-  accept: () => void;
-  cancel: () => void;
-  settings: object;
 }
 
 const modalRoot = document.getElementById('modal-root');
 
 const Modal: FunctionComponent<Props> = (props: Props): JSX.Element => {
-  const {
-    children, title, accept, cancel,
-  } = props;
+  const { children } = props;
   const container = document.createElement('div');
   container.setAttribute('id', 'modal-container');
-  container.onclick = cancel;
+
   const modal = (
-    <Presentational
-      title={title}
-      accept={accept}
-      cancel={cancel}
-    >
+    <Presentational>
       {children}
     </Presentational>
   );
--- src/components/Modal/presentational.tsx
+++ src/components/Modal/presentational.tsx
@@ -2,37 +2,13 @@ import React, { FunctionComponent } from 'react';
 
 interface Props {
   children: JSX.Element;
-  title: string;
-  accept: () => void;
-  cancel: () => void;
 }
 
 const Presentational: FunctionComponent<Props> = (props: Props): JSX.Element => {
-  const {
-    children, title, accept, cancel,
-  } = props;
+  const { children } = props;
   return (
     <div id="modal-content">
-      <h1 className="title">{title}</h1>
-      <div>
-        {children}
-      </div>
-      <div id="modal-buttons">
-        <button
-          type="button"
-          id="modal-accept"
-          onClick={accept}
-        >
-          Accept
-        </button>
-        <button
-          type="button"
-          id="modal-cancel"
-          onClick={cancel}
-        >
-          Cancel
-        </button>
-      </div>
+      {children}
     </div>
   );
 };
