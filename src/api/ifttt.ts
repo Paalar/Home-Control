eddc41aea18b43074a4b3c165607a0a4ce42f1d2
Paalar <paaledward@gmail.com>
2019-11-13 14:23:51 +0100

    Update ifttt api to include brewing

--- src/api/ifttt.ts
+++ src/api/ifttt.ts
@@ -1,9 +1,13 @@
-import * as RoutineApi from '../constants/routineApi';
+import { iftttKey } from '../constants/secrets';
 
-export const brewCoffeeEvent = (): Promise<void> => (
-  fetch(RoutineApi.brewCoffeeEventUrl)
+const getEventByKey = (event: string): string => (
+  `https://maker.ifttt.com/trigger/${event}/with/key/${iftttKey}`
+);
+
+export const ifttEvent = (key: string): string => getEventByKey(key);
+
+export const startEvent = (url: string): Promise<void> => (
+  fetch(url)
     .then((response) => console.log(response))
     .catch((error) => console.log(error))
 );
-
-export const lol = 2;
