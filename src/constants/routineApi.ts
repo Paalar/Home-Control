82046011128990eb3e67dc048d6f4c5e21404e6a
Paalar <paaledward@gmail.com>
2019-11-13 14:23:23 +0100

    Move constants between files

--- src/constants/routineApi.ts
+++ src/constants/routineApi.ts
@@ -1,13 +1,3 @@
-import { iftttKey } from './secrets';
-
-// IFTTT
-const getEventByKey = (event: string): string => (
-  `https://maker.ifttt.com/trigger/${event}/with/key/${iftttKey}`
-);
-const brewCoffeeKey = 'brew_coffee_for_2';
-
-export const brewCoffeeEventUrl = getEventByKey(brewCoffeeKey);
-
 // Spotify
 export const spotifyPauseUrl = 'https://api.spotify.com/v1/me/player/pause';
 export const spotifyResumeUrl = 'https://api.spotify.com/v1/me/player/play';
